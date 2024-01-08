7b07530b0b088a85d1a003f30ba3f7e38225a4a1
HOME\kim.nakhyun <kim_nakhyun@radcns.com>
2024-01-08 16:25:55 +0900

    24-01-08 16:25

--- in_module/path.js
+++ in_module/path.js
@@ -2,5 +2,25 @@ const path = require('path');
 
 const string = __filename;
 
-console.log('path.sep               : ', path.sep)
-console.log('path.delimiter         : ', path.delimiter)
+console.log('path.sep                   : ', path.sep);
+console.log('path.delimiter             : ', path.delimiter);
+console.log('======================================================================');
+console.log('path.dirname               : ', path.dirname(string));
+console.log('path.extname               : ', path.extname(string));
+console.log('path.basename              : ', path.basename(string));
+console.log('path.basename - extname    : ', path.basename(string, path.extname(string)));
+console.log('======================================================================');
+console.log('path.parse                 : ', path.parse(string));
+console.log('path.format                : ', path.format({
+    dir: 'C:\\s\\nst',
+    name: 'path',
+    ext: '.js'
+}));
+console.log('path.normalize             : ', path.normalize('C:\\s\\\\s\\\\nst'));
+console.log('======================================================================');
+console.log('path.isAbsolute(C:)        : ', path.isAbsolute('C:\\'));
+console.log('path.isAbsolute(./home)    : ', path.isAbsolute('./home'));
+console.log('======================================================================');
+console.log('path.relative              : ', path.relative('C:\\s\\nst\\in_module\\path.js', 'C:\\'));
+console.log('path.join                  : ', path.join(__dirname, '..', '..', '\\s', '.', '\\nst'));
+console.log('path.resolve               : ', path.resolve(__dirname, '..', 's', '.', '\\nst'));
---
+++ url/searchParams.js
@@ -0,0 +1,13 @@
+const myURL = new URL('http://gilbut.co.kr/?page=36&limit=10&category=nodejs&category=javascript');
+
+console.log('searchParams               : ', myURL.searchParams);
+console.log('searchParams.getAll        : ', myURL.searchParams.getAll('category'));
+console.log('searchParams.get           : ', myURL.searchParams.get('limit'));
+console.log('searchParams.has           : ', myURL.searchParams.has('page'));
+
+console.log('searchParams.keys          : ', myURL.searchParams.keys());
+console.log('searchParams.values        : ', myURL.searchParams.values());
+
+myURL.searchParams.append('filter', 'es3');
+myURL.searchParams.append('filter', 'es5');
+console.log(myURL.searchParams.getAll('filter'));
